c9678ee2020ecd4631eec1979ce5ad6a3eb6a24f
Ankur Sheel <ankur.sheel@gmail.com>
2019-08-07 11:13:03 +1200

    add a mousedown listener

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 
 interface CanvasProps {
     width: number;
@@ -7,6 +7,40 @@ interface CanvasProps {
 
 const Canvas = ({ width, height }: CanvasProps) => {
     const canvasRef = useRef<HTMLCanvasElement>(null);
+    const [isPainting, setIsPainting] = useState(false);
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const startPaint = useCallback((event: MouseEvent) => {
+        const coordinates = getCoordinates(event);
+        if (coordinates) {
+            setIsPainting(true);
+            setX(coordinates[0]);
+            setY(coordinates[1]);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        canvas.addEventListener('mousedown', startPaint);
+        return () => {
+            canvas.removeEventListener('mousedown', startPaint);
+        };
+    }, [startPaint]);
+
+    const getCoordinates = (event: MouseEvent) => {
+        if (!canvasRef.current) {
+            return;
+        }
+
+        if (['mousedown', 'mousemove'].includes(event.type)) {
+            const canvas: HTMLCanvasElement = canvasRef.current;
+            return [event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop];
+        }
+    };
 
     return <canvas ref={canvasRef} height={height} width={width} />;
 };
