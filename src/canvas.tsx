3d56f0a0a03f287ab4be1504be35d3e0102e2218
Ankur Sheel <ankur.sheel@gmail.com>
2019-08-07 11:44:41 +1200

    draw line on mouse move

--- src/canvas.tsx
+++ src/canvas.tsx
@@ -31,6 +31,29 @@ const Canvas = ({ width, height }: CanvasProps) => {
         };
     }, [startPaint]);
 
+    const paint = useCallback(
+        (event: MouseEvent) => {
+            if (isPainting) {
+                let [newX, newY] = getCoordinates(event) || [0, 0];
+                drawLine(x, y, newX, newY);
+                setX(newX);
+                setY(newY);
+            }
+        },
+        [isPainting, x, y]
+    );
+
+    useEffect(() => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        canvas.addEventListener('mousemove', paint);
+        return () => {
+            canvas.removeEventListener('mousemove', paint);
+        };
+    }, [paint]);
+
     const getCoordinates = (event: MouseEvent) => {
         if (!canvasRef.current) {
             return;
@@ -39,6 +62,26 @@ const Canvas = ({ width, height }: CanvasProps) => {
         if (['mousedown', 'mousemove'].includes(event.type)) {
             const canvas: HTMLCanvasElement = canvasRef.current;
             return [event.pageX - canvas.offsetLeft, event.pageY - canvas.offsetTop];
+        }
+    };
+
+    const drawLine = (firstX: number, firstY: number, secondX: number, secondY: number) => {
+        if (!canvasRef.current) {
+            return;
+        }
+        const canvas: HTMLCanvasElement = canvasRef.current;
+        const context = canvas.getContext('2d');
+        if (context) {
+            context.strokeStyle = 'red';
+            context.lineJoin = 'round';
+            context.lineWidth = 5;
+
+            context.beginPath();
+            context.moveTo(secondX, secondY);
+            context.lineTo(firstX, firstY);
+            context.closePath();
+
+            context.stroke();
         }
     };
 
